8b6c16aaaa56aa720d3015fedaa273692c4a9ae8
Tejesh@PRDXN <tejeshs@prdxn.com>
2020-02-25 17:37:22 +0530

    modified js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,44 @@
 'use strict';
 
-var button = document.querySelectorAll(".tabs-controls li");
-console.log(button);
-var tab=[];
+// window.onload = function () {
 
-for (var i = 0; i < button.length; i++) {
-  tab.push(button[i]);
-}
+//   var button = document.querySelectorAll(".tabs-controls li");
+//   var tab = [];
 
-for (var i = 0; i < button.length; i++){
-    button[i].onclick = function(){
-      var index = tab.indexOf(this);
-      index += 1;
-      var tabContent = document.querySelector(".tabs-content ul li:nth-child("+index+")");
-      
-      var active = tabContent.classList.contains("active-tab");
+//   for (var i = 0; i < button.length; i++) {
+//     tab.push(button[i]);
+//   }
 
-      if(!active){
-        var currentActive = document.querySelector(".active-tab");
-        currentActive.classList.remove("active-tab");
-        var currentActiveButton = document.querySelector(".active");
-        currentActiveButton.classList.remove("active");
+//   for (var i = 0; i < button.length; i++) {
+//     button[i].onclick = function () {
+//       var index = tab.indexOf(this);
+//       index += 1;
+//       var tabContent = document.querySelector(".tabs-content ul li:nth-child(" + index + ")");
 
-        console.log(this);
-        this.classList.add("active");
-        tabContent.classList.add("active-tab");
-      }
-    }
-}
+//       var active = tabContent.classList.contains("active-tab");
+
+//       if (!active) {
+//         var currentActive = document.querySelector(".active-tab");
+//         currentActive.classList.remove("active-tab");
+//         var currentActiveButton = document.querySelector(".active");
+//         currentActiveButton.classList.remove("active");
+        
+//         this.classList.add("active");
+//         tabContent.classList.add("active-tab");
+//       }
+//     }
+//   }
+// }
+
+
+$(document).ready(function () {
+
+  $(".tabs-controls li").click(function(){
+    $(this).addClass("active").siblings().removeClass("active");
+    var a = $(this).attr("data-filter");
+    $("." + a).addClass("active-tab").siblings().removeClass("active-tab");
+  });
+});
 
 
 
